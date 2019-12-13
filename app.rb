667f6b9b633ca97bb24a458abf4e78b3184a879b
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-13 03:08:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
     @result = @phrase * @number
   end
   
-  # get '/say/:word1/:word2/:word3/:word4/:word5' do
-  # end
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+  end
   
   # get '/:operation/:number1/:number2' do
   # end
