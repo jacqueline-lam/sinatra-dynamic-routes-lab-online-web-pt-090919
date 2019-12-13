9789be1d9e77cc187e5e83c5f3ecdc95be80f375
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-13 02:59:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,6 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @result = @phase * @number
-    
   end
   
   # get '/say/:word1/:word2/:word3/:word4/:word5' do
