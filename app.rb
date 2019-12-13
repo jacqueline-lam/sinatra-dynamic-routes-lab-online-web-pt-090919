a0d3afad92e9e12f653b91ab55d310f9fa8b6823
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-13 03:12:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
   
-  # adds two numbers together
+  # subtract two numbers together
   get '/:operation/:number1/:number2' do
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
